80957f14ea49f3839adfc604724829a30df29fd6
Tameem Bin Haider <haidertameem@gmail.com>
2024-04-28 11:54:03 +0600

    refactor: create a generic link props getter

--- app/_components/navigation.tsx
+++ app/_components/navigation.tsx
@@ -7,33 +7,23 @@ import { ModeToggle } from './theme-toggle'
 
 export function Navigation() {
   const pathname = usePathname()
-  const getClasses = (path: string) =>
-    cn(
+
+  const getLinkProps = (path: string) => ({
+    className: cn(
       'text-sm font-medium transition-colors hover:text-primary',
       pathname !== path && 'text-muted-foreground'
-    )
+    ),
+    href: path
+  })
+
   return (
     <div className="border-b px-2 py-1">
       <div className="mx-auto flex max-w-6xl items-center justify-between">
         <nav className="flex gap-2">
-          <Link className={getClasses('/')} href="/">
-            My questions
-          </Link>
-          <Link
-            className={getClasses('/create-question')}
-            href="/create-question"
-          >
-            Create question
-          </Link>
-          <Link
-            className={getClasses('/review-questions')}
-            href="/review-questions"
-          >
-            Review questions
-          </Link>
-          <Link className={getClasses('/question-bank')} href="/question-bank">
-            Question bank
-          </Link>
+          <Link {...getLinkProps('/')}>My questions</Link>
+          <Link {...getLinkProps('/create-question')}>Create question </Link>
+          <Link {...getLinkProps('/review-questions')}>Review questions</Link>
+          <Link {...getLinkProps('/question-bank')}>Question bank</Link>
         </nav>
         <ModeToggle />
       </div>
