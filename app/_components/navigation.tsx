42c0481ed164c2e8f0412574981d435005c46644
Tameem Bin Haider <haidertameem@gmail.com>
2024-05-01 11:41:30 +0600

    feat: add app title

--- app/_components/navigation.tsx
+++ app/_components/navigation.tsx
@@ -4,6 +4,8 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import { ModeToggle } from './theme-toggle'
+import { Book } from 'lucide-react'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
 
 export function Navigation() {
   const pathname = usePathname()
@@ -17,15 +19,24 @@ export function Navigation() {
   })
 
   return (
-    <div className="border-b px-2 py-1">
-      <div className="mx-auto flex max-w-6xl items-center justify-between">
+    <div className="flex items-center justify-between gap-2 border-b px-2 py-1">
+      <div className="flex w-44 flex-initial items-center gap-2">
+        <Book className="h-8 w-8" />
+        <span className="text-lg">Question Bank</span>
+      </div>
+      <div className="flex items-center justify-center">
         <nav className="flex gap-2">
           <Link {...getLinkProps('/')}>My questions</Link>
           <Link {...getLinkProps('/create-question')}>Create question </Link>
           <Link {...getLinkProps('/review-questions')}>Review questions</Link>
           <Link {...getLinkProps('/question-bank')}>Question bank</Link>
         </nav>
+      </div>
+      <div className="flex w-44 flex-initial justify-end gap-2">
         <ModeToggle />
+        <Avatar>
+          <AvatarFallback>U</AvatarFallback>
+        </Avatar>
       </div>
     </div>
   )
