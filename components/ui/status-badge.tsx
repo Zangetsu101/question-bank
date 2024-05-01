3773f1058c913671441ec9cf526d34856eb83928
Tameem Bin Haider <haidertameem@gmail.com>
2024-05-01 11:49:09 +0600

    fix: use darker colors for status badge

--- components/ui/status-badge.tsx
+++ components/ui/status-badge.tsx
@@ -1,14 +1,15 @@
 import { Badge } from '@/components/ui/badge'
 import { Status } from '@/db/schema'
+import { cn } from '@/lib/utils'
 
 export function StatusBadge(props: { status: Status }) {
   return (
     <Badge
-      className={
-        props.status === 'in-review'
-          ? 'bg-yellow-500 hover:bg-yellow-500/80'
-          : 'bg-green-500 hover:bg-green-500/80'
-      }
+      className={cn(
+        props.status === 'in-review' && 'bg-yellow-700 hover:bg-yellow-700/80',
+        props.status === 'accepted' && 'bg-green-700 hover:bg-green-700/80',
+        'text-white'
+      )}
     >
       {props.status}
     </Badge>
